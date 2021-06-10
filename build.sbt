6061c1733e57fb22c0fc8a7b1af5b003bdc8dc36
Chinkit Patel <chinkit@Chinkits-MBP.lan>
2021-06-11 00:15:55 +0930

    Creating a basic delta table

--- build.sbt
+++ build.sbt
@@ -7,4 +7,5 @@ scalaVersion := "2.12.14"
 idePackagePrefix := Some("au.com.aeonsoftware")
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.1.2"
 libraryDependencies += "org.apache.spark" % "spark-streaming_2.12" % "3.1.2"
+libraryDependencies += "io.delta" %% "delta-core" % "1.0.0"
 
--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,18 +1,41 @@
 package au.com.aeonsoftware
-import org.apache.spark.sql.SparkSession
+import org.apache.commons.io.FileUtils
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.types.StructType
+
+import java.io.File
+
 
 object App {
   def main(args: Array[String]): Unit = {
 
+    val DELTA_TABLE_NAME = "./data/delta-store/music-table"
+
     val spark = SparkSession
       .builder()
       .appName("Delta 101")
-      .master("local")
+      .master("local[*]")
+      .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+      .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
       .getOrCreate()
 
-    val df = spark.read.json("./data/*.json")
+    val musicReviewSchema = new StructType()
+      .add("reviewerID", "string")
+      .add("asin", "string")
+      .add("reviewerName", "string")
 
-    df.show()
+    val data = spark
+      .read
+      .schema(musicReviewSchema)
+      .json("./data/*.json")
+
+    val file = new File(DELTA_TABLE_NAME)
+    if (file.exists()) FileUtils.deleteDirectory(file)
+
+    data
+      .write
+      .format("delta")
+      .save(DELTA_TABLE_NAME)
 
   }
 }
