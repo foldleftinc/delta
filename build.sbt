bdd3b58cc12862418d997cd8173e349e0c6a4969
Chinkit Patel <chinkit@Chinkits-MBP.lan>
2021-06-10 23:17:51 +0930

    Adding spark

--- build.sbt
+++ build.sbt
@@ -5,3 +5,6 @@ version := "0.1"
 scalaVersion := "2.12.14"
 
 idePackagePrefix := Some("au.com.aeonsoftware")
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.1.2"
+libraryDependencies += "org.apache.spark" % "spark-streaming_2.12" % "3.1.2"
+
--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,7 +1,18 @@
 package au.com.aeonsoftware
+import org.apache.spark.sql.SparkSession
 
 object App {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+
+    val spark = SparkSession
+      .builder()
+      .appName("Delta 101")
+      .master("local")
+      .getOrCreate()
+
+    val df = spark.read.json("./data/*.json")
+
+    df.show()
+
   }
 }
