7e2ff59c5b074ef6a0403483ac6a141d28a4c586
Chinkit Patel <chinkit@Chinkits-MacBook-Pro.local>
2021-06-11 14:41:10 +0930

    bootstrapping with pg debezium kconnect and kafka

---
+++ conf/init-data.sql
@@ -0,0 +1,28 @@
+-- Create database
+CREATE DATABASE DeltaSample;
+
+-- Creation of table Location
+CREATE TABLE IF NOT EXISTS Location (
+  Id INT NOT NULL,
+  Name varchar(450) NOT NULL,
+  PRIMARY KEY (Id)
+);
+
+-- Creation of table person
+CREATE TABLE IF NOT EXISTS Person (
+  Id INT NOT NULL,
+  LocationId INT NOT NULL,
+  Name varchar(250) NOT NULL,
+  PRIMARY KEY (Id),
+  CONSTRAINT fk_location
+      FOREIGN KEY(LocationId)
+	  REFERENCES Location(Id)
+);
+
+-- Insert starting data
+INSERT INTO Location (Id, Name) VALUES (1, 'Sydney');
+INSERT INTO Location (Id, Name) VALUES (2, 'Adelaide');
+
+INSERT INTO Person (Id, LocationId, Name) VALUES (1, 1, 'Mark');
+INSERT INTO Person (Id, LocationId, Name) VALUES (2, 1, 'Chinkit');
+INSERT INTO Person (Id, LocationId, Name) VALUES (3, 1, 'Troy');
