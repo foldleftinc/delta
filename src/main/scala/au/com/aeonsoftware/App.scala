71df8d2e89c7387dc04d6caa6d8110d40091e340
Chinkit Patel <chinkit@Chinkits-MBP.lan>
2021-06-13 01:50:42 +0930

    Person bronze delta table working

--- src/main/scala/au/com/aeonsoftware/App.scala
+++ src/main/scala/au/com/aeonsoftware/App.scala
@@ -1,6 +1,7 @@
 package au.com.aeonsoftware
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, explode, lit}
 import org.apache.spark.sql.streaming.Trigger
 
 object App {
@@ -29,6 +30,8 @@ object App {
     val inputSchema = inputEncoder.schema
 
     val inputDF = spark
+//      .read
+//      .text(SAMPLE_FILE_PATH)
       .readStream
       .format("kafka") // org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.5
       .option("kafka.bootstrap.servers", KAFKA_BOOTSTRAP_SERVERS)
@@ -42,12 +45,37 @@ object App {
       .selectExpr("CAST(value AS STRING)")
       .select(functions.from_json($"value", inputSchema).as("data"))
 
+    //TODO: DRY fix
+    val beforeDF = extractedDF
+      .select(col("data.payload.op"),
+        col("data.payload.source.ts_ms"),
+        col("data.payload.source.lsn"),
+        col("data.payload.before") as 'person)
+      .withColumn("image",lit("before"))
+      .where("data.payload.before IS NOT NULL")
 
-    val query = extractedDF
+    val afterDF = extractedDF
+      .select(col("data.payload.op"),
+        col("data.payload.source.ts_ms"),
+        col("data.payload.source.lsn"),
+        col("data.payload.after"))
+      .withColumn("image",lit("after"))
+      .where("data.payload.after IS NOT NULL")
+
+    val personDF = beforeDF.unionAll(afterDF)
+      .select($"person.id",$"person.locationid",$"person.name",$"op",$"ts_ms",$"lsn",$"image")
+
+    val query = personDF
       .writeStream
       .trigger(Trigger.ProcessingTime("5 seconds"))
+      .foreachBatch { (batchDF: DataFrame, batchID: Long) =>
+        println(s"Writing to Delta $batchID")
+        batchDF.write
+          .format("delta")
+          .mode("append")
+          .save(DELTA_TABLE_ROOT_PATH + "bronze-person")
+      }
       .outputMode("update")
-      .format("console")
       .start()
 
     query.awaitTermination()
